554a05f65ac906ab19d92664733ca9e8ac5c24b1
Vivek Thakur <20509.vivek@gpranchi.org>
2024-10-14 22:25:58 +0530

    commit

---
+++ Javascript/01_variables.js
@@ -0,0 +1,13 @@
+// VARIABLES
+// Variables are containers for storing data values. In javascript we can use var, let, const keywords to declare variables.
+// Syntax for declaring variables: ```var variableName = value;```
+
+// Example 1:
+var x = 5;
+
+console.log(x);
+
+// 1. ```var``` keyword is used to declare a variable.
+// 2. ```x``` is the variable name.
+
+// Understanding var keyword:
