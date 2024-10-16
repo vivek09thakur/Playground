86bb7b6f5b14c04605a248ebf95014c6122401f8
Vivek Thakur <20509.vivek@gpranchi.org>
2024-10-16 22:09:40 +0530

    commit

--- Javascript/01_variables.js
+++ Javascript/01_variables.js
@@ -1,13 +1,27 @@
-// VARIABLES
-// Variables are containers for storing data values. In javascript we can use var, let, const keywords to declare variables.
-// Syntax for declaring variables: ```var variableName = value;```
+// VARIABLES :
+// Variables are containers for storing data values. In javascript we can use var, let, const keywords to declare variables. Syntax for declaring variables: ```var variableName = value;```
 
 // Example 1:
+console.log("\n\nUnderstanding variables in JavaScript");
+console.log("\n1. Understanding var keyword: ");
+
+console.log("\nExample 1: ");
 var x = 5;
 
-console.log(x);
+console.log("\nValue of x =", x);
 
 // 1. ```var``` keyword is used to declare a variable.
 // 2. ```x``` is the variable name.
 
-// Understanding var keyword:
+// Understanding ```var```` keyword:
+// Variables decalred using ``var`` keyword are function scoped and not block scoped. Meaning, the variable is available throughout the function in which it is declared.
+
+// Example 2:
+function test_function_1() {
+  var y = 10;
+  console.log("Inside function: ", y);
+}
+
+test_function_1();
+
+// console.log("Outside function: ", y); // ReferenceError: y is not defined, bcoz y is not available outside the function.
