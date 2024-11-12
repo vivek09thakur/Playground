8ff9b0012a0c71446805cc7a6be64a937eb1ff48
vivek09thakur <vivekthakurcse20509@gmail.com>
2024-11-12 23:18:24 +0530

    commit

---
+++ Javascript/03_datatypes.js
@@ -0,0 +1,19 @@
+// Data Types in javascript
+
+// 1. Integers
+
+// userID = 2345678; // Never do this? why? 
+//  It is a bad practice to use a variable without declaring it. It will be treated as a global variable.
+
+const userID = 2345678;
+
+console.log("User ID: ", userID);
+console.log("Type of User ID : ", typeof(userID));
+
+// 2. Float
+
+// const  userRating = BigInt(522345678901234567890);
+const valueofpie = Math.PI;
+
+console.log("Value of Pie: ", valueofpie);
+console.log("Type of Pie: ", typeof(valueofpie));
