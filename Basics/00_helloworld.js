80ebb6171635a4997319251d67948f676ae11c14
vivek09thakur <vivekthakurcse20509@gmail.com>
2024-12-06 21:01:11 +0530

    commit

---
+++ Basics/00_helloworld.js
@@ -0,0 +1,17 @@
+// Hello world
+// console.log("Hello World");
+
+// To console any output we use console.log (or console.error, console.warn, console.table for different types of output)
+console.log("Starting off all over again");
+// This will display any warning in the console
+console.warn("This is a warning");
+// This will display any error in the console
+console.error("This might be buggy");
+// This will display the output in a table format
+console.table(
+    {
+        name: "John Doe",
+        age: 30,
+        address: "123 Main St"
+    }
+)
