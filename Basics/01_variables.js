6df1e43fde279e0a1bb1afdf8c17cbb661db2da4
vivek09thakur <vivekthakurcse20509@gmail.com>
2024-12-06 23:41:04 +0530

    commit

---
+++ Basics/01_variables.js
@@ -0,0 +1,8 @@
+// Variables? --> Variables are containers for storing data values.
+
+// 1. Declaring a variable : we use (let, var, const) keywords to declare a variable
+
+let name = "Vivek Thakur";
+console.log("Name :",name," Variable Type: " ,typeof(name));
+let age = 20;
+console.log("Age :",age);
